a2b244be4985c0f944e5fb59d3d96344af5db5b3
bj-ranjan <barmanranjanjyoti@gmail.com>
2023-08-04 13:51:42 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
           Welcome to Git.....
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
